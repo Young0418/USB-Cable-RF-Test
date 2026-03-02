9e7cfb0e905e4718986ff582f489b7ab5f2bdc1f
Young0418 <1550672688@qq.com>
2026-03-02 23:08:10 +0800

    硬件部分新增了把s参数复数形式转化为dB形式的代码部分

--- hardware_comm.py
+++ hardware_comm.py
@@ -1,6 +1,7 @@
 import pyvisa
 import time
 import random
+import math
 
 def get_s_params():
     VISA_ADDR = "TCPIP0::127.0.0.1::5025::SOCKET"
@@ -9,6 +10,9 @@ def get_s_params():
     inst.timeout = 10000
     inst.write_termination = '\n'
     inst.read_termination = '\n'
+
+    inst.write("FORMAT REAL")
+    inst.write(":SENSe:SWEep:POINts 1001")
 
     CONFIG={
         "仪器信息": inst.query("*IDN?").strip(),
@@ -30,30 +34,56 @@ def get_s_params():
             "点数":1001
         },
     }
+    raw_s11 = inst.quary("CALC:DATA? S11").strip().split(',')
+    #解析为dB值
+    s11_dB = []
+    for i in (0,len(raw_s11),2):
+        real=float(raw_s11[i])
+        imag=float(raw_s11[i+1])
+        mag=math.sqrt(real*real+imag*imag) #幅度
+        #避免取0对数，加极小值
+        if mag<=0:
+            dB=-200
+        else:
+            dB=20*math.log10(mag)
+            s11_dB.append(dB)
 
-    hardware_data = {
-        "配置参数":CONFIG,
-        "S参数": {
-            "S11": list(map(float, inst.query("CALC:DATA? S11").strip().split(','))),
-            "S21": list(map(float, inst.query("CALC:DATA? S21").strip().split(',')))
-        }
-    }
+    raw_s21 = inst.quary(f"CALC:DATA? S21").strip().split(',')
+    s21_dB = []
+    for i in (0,len(raw_s21),2):
+        real=float(raw_s21[i])
+        imag=float(raw_s21[i+1])
+        mag=math.sqrt(real*real+imag*imag)
+        if mag<=0:
+            dB=-200
+        else:
+            dB=20*math.log10(mag)
+            s21_dB.append(dB)
 
     inst.close()
     rm.close()
 
     # 1. 提取测试点列表（和S11长度一致）
-    test_points = list(range(1, len(hardware_data["S参数"]["S11"])+1))
+    start_freq=CONFIG["测试参数"]["起始频率(Hz)"]
+    stop_freq=CONFIG["测试参数"]["终止频率(Hz)"]
+    num_points=CONFIG["测试参数"]["点数"]
+    freq_list=[]
+    if num_points >1:
+        step=(stop_freq-start_freq)/(num_points-1)
+        for i in range(num_points):
+            freq_list.append(start_freq+i*step)
+    else:
+        freq_list=start_freq
     # 2. 按hardware_protocol格式组装返回字典
     return {
-        "S11": hardware_data["S参数"]["S11"],  # 保留S11一维列表
-        "S21": hardware_data["S参数"]["S21"],  # 保留S21一维列表
+        "S11": s11_dB,  # 保留S11一维列表
+        "S21": s21_dB,  # 保留S21一维列表
         "device_info": {  # 严格按协议：model/cable_type/test_time
-            "model": hardware_data["配置参数"]["仪器信息"],
-            "cable_type": hardware_data["配置参数"]["线缆信息"]["类型"],
-            "test_time": hardware_data["配置参数"]["测试环境"]["测试时间"]
+            "model": CONFIG["仪器信息"],
+            "cable_type": CONFIG["线缆信息"]["类型"],
+            "test_time": CONFIG["测试环境"]["测试时间"]
         },
-        "test_points": test_points  # 新增：测试点列表
+        "test_points": freq_list  # 新增：测试点列表
     }
 
 if __name__ == "__main__":
